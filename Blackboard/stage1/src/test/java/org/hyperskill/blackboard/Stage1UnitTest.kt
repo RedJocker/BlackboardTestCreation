453c0bf8352e8509d7e84c4e47d96144ddcb9229
RedJocker <mauricio.guaruja@gmail.com>
2023-06-12 19:14:38 -0300

    add assertions checking valid login with default values

--- Blackboard/stage1/src/test/java/org/hyperskill/blackboard/Stage1UnitTest.kt
+++ Blackboard/stage1/src/test/java/org/hyperskill/blackboard/Stage1UnitTest.kt
@@ -1,6 +1,7 @@
 package org.hyperskill.blackboard
 
 import org.hyperskill.blackboard.internals.BlackboardUnitTest
+import org.hyperskill.blackboard.internals.backend.database.MockUserDatabase
 import org.hyperskill.blackboard.internals.screen.LoginScreen
 import org.junit.FixMethodOrder
 import org.junit.Test
@@ -44,6 +45,32 @@ class Stage1UnitTest : BlackboardUnitTest<MainActivity>(MainActivity::class.java
         testActivity {
             LoginScreen(this, initViews = false).apply {
                 loginSubmitBt
+            }
+        }
+    }
+
+    @Test
+    fun test04_checkValidSubmissionRoleTeacherDefaultUserMap() {
+        val teacher = MockUserDatabase.users[MockUserDatabase.GEORGE]!!
+
+        testActivity {
+            LoginScreen(this).apply {
+                fillLogin(teacher.username, teacher.plainPass)
+                val caseDescription = "With default userMap and correct teacher ${teacher.username} login"
+                assertToastTeacherLoginSuccess(teacher.username, caseDescription)
+            }
+        }
+    }
+
+    @Test
+    fun test05_checkValidSubmissionRoleStudentDefaultUserMap() {
+        val student = MockUserDatabase.users[MockUserDatabase.LUCAS]!!
+
+        testActivity {
+            LoginScreen(this).apply {
+                fillLogin(student.username, student.plainPass)
+                val caseDescription = "With default userMap and correct student ${student.username} login"
+                assertToastStudentLoginSuccess(student.username, caseDescription)
             }
         }
     }
