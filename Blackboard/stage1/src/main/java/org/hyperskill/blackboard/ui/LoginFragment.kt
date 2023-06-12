976a412e03fe13c2fd7b16ec2655cbb7da5bd2d9
RedJocker <mauricio.guaruja@gmail.com>
2023-06-12 14:50:15 -0300

    stage1 reference solution

--- Blackboard/stage1/src/main/java/org/hyperskill/blackboard/ui/LoginFragment.kt
+++ Blackboard/stage1/src/main/java/org/hyperskill/blackboard/ui/LoginFragment.kt
@@ -7,16 +7,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.addTextChangedListener
-import org.hyperskill.blackboard.R
 import org.hyperskill.blackboard.databinding.BlackboardTitleBinding
 import org.hyperskill.blackboard.databinding.FragmentLoginBinding
 import org.hyperskill.blackboard.util.Extensions.showToast
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
 
 
 class LoginFragment : Fragment() {
 
     lateinit var loginBinding: FragmentLoginBinding
     lateinit var titleBinding: BlackboardTitleBinding
+
+    val defaultUserMap = mapOf(
+            "George" to ("TEACHER" to "A6xnQhbz4Vx2HuGl4lXwZ5U2I8iziLRFnhP5eNfIRvQ="), // plainPass = 1234
+            "Lucas" to ("STUDENT" to "SfjRCkJKMPlfohEJae0FOjNlJYbaGQ++tcY3LWnX40Q="),  // plainPass = 32A1
+    )
+
+    @Suppress("DEPRECATION", "UNCHECKED_CAST")
+    val userMap by lazy(LazyThreadSafetyMode.NONE) {
+        requireActivity().intent
+                .getSerializableExtra("userMap") as? Map<String, Pair<String, String>>
+                ?: defaultUserMap
+    }
 
     private val clearEtError: (text: Editable?) -> Unit = {
         println("clearEtError")
@@ -26,7 +39,6 @@ class LoginFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         println("LoginFragment.onCreateView")
-
         loginBinding = FragmentLoginBinding.inflate(inflater, container, false)
         titleBinding = BlackboardTitleBinding.bind(loginBinding.root)
         return loginBinding.root
@@ -36,26 +48,26 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         println("LoginFragment.onViewCreated")
 
-        titleBinding.blackboardTitle.apply {
-            error = "NetworkError"
-            isFocusableInTouchMode = true
-            isFocusable = true
-        }
-
 
         loginBinding.apply {
 
             loginSubmitBtn.setOnClickListener {
                 println("click loginSubmitBtn")
-                titleBinding.blackboardTitle.requestFocus()
-                val isOk = loginPassEt.text.isNotBlank() && loginPassEt.text.isNotBlank()
-                context?.also{ context ->
-                    if(isOk) {
-                        context.showToast("login ok")
+
+                val username = loginUsernameEt.text.toString()
+                val plainPass = loginPassEt.text.toString()
+
+                if(userMap.containsKey(username)) {
+                    val (role, storedPass) = userMap[username]!!
+                    if(encryptPass(plainPass) == storedPass) {
+                        onValidLogin(username, role)
                     } else {
-                        loginUsernameEt.error = "login failed"
+                        onInvalidLogin()
                     }
+                } else {
+                    onInvalidLogin()
                 }
+
             }
 
             loginUsernameEt.addTextChangedListener(afterTextChanged = clearEtError)
@@ -63,5 +75,36 @@ class LoginFragment : Fragment() {
         }
 
         println(loginBinding)
+    }
+
+    private fun onValidLogin(username: String, role: String) {
+        val message = when (role) {
+            "STUDENT" -> "Hello $username"
+            "TEACHER" -> "Good day teacher $username"
+            else -> throw IllegalArgumentException("Invalid Role $role")
+        }
+        context!!.showToast(message)
+        clearUserInput()
+    }
+
+    private fun clearUserInput() {
+        loginBinding.loginPassEt.setText("")
+        loginBinding.loginUsernameEt.setText("")
+    }
+
+    private fun onInvalidLogin() {
+        loginBinding.loginPassEt.setText("")
+        loginBinding.loginUsernameEt.error = "Invalid Login"
+        loginBinding.loginUsernameEt.requestFocus()
+    }
+
+    fun encryptPass(pass: String): String {
+        val rawPassBytes = pass.toByteArray(StandardCharsets.UTF_8)
+        val messageDigest = MessageDigest.getInstance("SHA-256")
+        val sha256HashPass = messageDigest.digest(rawPassBytes)
+        val base64sha256HashPass = android.util.Base64.encodeToString(
+                sha256HashPass, android.util.Base64.NO_WRAP
+        )
+        return base64sha256HashPass
     }
 }
