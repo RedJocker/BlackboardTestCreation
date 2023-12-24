d77b16fb2c809634ae0929073f02a7793f3808a4
Orifjon <orifjon.olimjonov@mail.ru>
2023-12-24 09:42:08 +0500

    some changed

--- Blackboard/stage3/src/test/java/org/hyperskill/blackboard/internals/screen/LoginScreen.kt
+++ Blackboard/stage3/src/test/java/org/hyperskill/blackboard/internals/screen/LoginScreen.kt
@@ -1,6 +1,7 @@
 package org.hyperskill.blackboard.internals.screen
 
 import android.app.Activity
+import android.text.InputType
 import android.widget.Button
 import android.widget.EditText
 import org.hyperskill.blackboard.internals.BlackboardUnitTest
@@ -26,10 +27,11 @@ class LoginScreen<T: Activity>(val test: BlackboardUnitTest<T>, initViews: Boole
     val loginUsernameEt: EditText by lazy {
         with(test) {
             activity.findViewByString<EditText>(LOGIN_USERNAME_ET_ID).apply {
+                val textPersonNameType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PERSON_NAME
                 assertValues(
                     expectedText = "",
                     expectedHint = "username",
-                    expectedInputType = 97,
+                    expectedInputType = textPersonNameType,
                     inputTypeString = "textPersonName",
                     idString = LOGIN_USERNAME_ET_ID,
                     caseDescription = DESCRIPTION_INITIALIZATION
@@ -40,10 +42,13 @@ class LoginScreen<T: Activity>(val test: BlackboardUnitTest<T>, initViews: Boole
     val loginPassEt: EditText by lazy {
         with(test) {
             activity.findViewByString<EditText>(LOGIN_PASS_ET_ID).apply {
+
+                val textPasswordType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+
                 assertValues(
                         expectedText = "",
                         expectedHint = "password",
-                        expectedInputType = 129,
+                        expectedInputType = textPasswordType,
                         inputTypeString = "textPassword",
                         idString = LOGIN_PASS_ET_ID,
                         caseDescription = DESCRIPTION_INITIALIZATION
